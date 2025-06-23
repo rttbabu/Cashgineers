adced400d48721e22ab293a385d05f6161e34354
ArynKeenan <arynkeenan@icloud.com>
2025-06-23 11:35:09 +0000

    New Hackathon Project

--- backend/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Initialize the application
-        System.out.println("Bank Dashboard Application is starting...");
-        
-        // Additional setup and configuration can be added here
-    }
-}
